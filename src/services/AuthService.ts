138bb7d890e38b4278e3878458f5ab472d92b41c
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-11-20 12:48:20 +0600

    add: cleanup service for query

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,54 +1,22 @@
 import UserModel from "../models/UserModel";
-import { success, failure } from "../utilities/common";
-import { generateRandomCode } from "../utilities/common";
-import { emailWithNodemailerGmail } from "../config/email.config";
-import bcrypt from "bcryptjs";
 
 class AuthService {
-  async signUp(
-    fullName: string,
-    email: string,
-    password: string,
-    confirmPassword: string
-  ) {
+  async signUp(fullName: string, email: string, password: string) {
     try {
-      const emailCheck = await UserModel.findOne({ email });
-
-      if (emailCheck) {
-        return failure("User with email already exists");
-      }
-
-      if (password !== confirmPassword) {
-        return failure("Passwords do not match");
-      }
-
-      const hashedPassword = await bcrypt.hash(password, 10);
       const user = await UserModel.create({
         fullName,
         email,
-        password: hashedPassword,
+        password,
       });
 
       if (!user) {
-        return failure("User registration failed");
+        return null;
       }
 
-      //   const verificationCode = generateRandomCode();
-      //   user.verificationCode = verificationCode;
-      //   await user.save();
-
-      //   const emailData: IEmailData = {
-      //     to: email,
-      //     subject: "Email Verification",
-      //     text: `Your verification code is: ${verificationCode}`,
-      //   };
-
-      //   await emailWithNodemailerGmail(emailData);
-
-      return success("User registered successfully", user);
+      return user;
     } catch (error) {
-      console.error("Error registering user:", error);
-      return failure("Error registering user");
+      console.error("Error registering user(AuthService signUp):", error);
+      return null;
     }
   }
 }
