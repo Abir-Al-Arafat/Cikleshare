b9b48e0b203c652829c1bf459861ba49c52d0e23
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-11-22 10:42:58 +0600

    cleanup: unnecessary imports

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,8 +1,4 @@
 import UserModel from "../models/UserModel";
-import { success, failure } from "../utilities/common";
-import { generateRandomCode } from "../utilities/common";
-import { emailWithNodemailerGmail } from "../config/email.config";
-import bcrypt from "bcryptjs";
 
 class UserService {
   async isEmailTaken(email: string): Promise<boolean> {
