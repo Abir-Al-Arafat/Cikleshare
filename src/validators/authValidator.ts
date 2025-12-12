30919a0a7d7154fc8f8ad59499911664d1f93e02
Abir-Al-Arafat <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-12-12 17:49:47 +0600

    validator: otp verification & reset password

--- src/validators/authValidator.ts
+++ src/validators/authValidator.ts
@@ -19,6 +19,34 @@ const authValidator = {
       .equals("passwordRecovery")
       .withMessage("purpose must be 'passwordRecovery'"),
   ],
+  verifyOtp: [
+    body("email").not().isEmpty().withMessage("email is required"),
+    body("otp")
+      .not()
+      .isEmpty()
+      .withMessage("otp is required")
+      .isLength({ min: 6, max: 6 })
+      .withMessage("otp must be 6 digits"),
+    body("purpose")
+      .not()
+      .isEmpty()
+      .withMessage("purpose is required")
+      .isIn(["passwordRecovery", "emailVerification", "twoFactorAuth"])
+      .withMessage("Invalid purpose value"),
+  ],
+  resetPassword: [
+    body("email").not().isEmpty().withMessage("email is required"),
+    body("newPassword")
+      .not()
+      .isEmpty()
+      .withMessage("newPassword is required")
+      .isLength({ min: 5 })
+      .withMessage("Password must be at least 5 characters"),
+    body("confirmPassword")
+      .not()
+      .isEmpty()
+      .withMessage("confirmPassword is required"),
+  ],
 };
 
 export default authValidator;
